d0e8b9f0ae59abdbfe7d124b1bf2188c826e0d68
Eric <eric.espie@combodo.com>
2020-12-03 17:53:13 +0100

    N°3431 - Change name of views attributes of conform to 2.6

--- module.combodo-views.php
+++ module.combodo-views.php
@@ -66,35 +66,47 @@ if (!class_exists('CombodoViewsInstaller'))
 			foreach (MetaModel::GetClasses('bizmodel') as $sClass)
 			{
 				$sView = MetaModel::DBGetView($sClass);
-				if (CMDBSource::IsTable($sView))
-				{
-					// Check that the view is complete
-					//
-					// Note: checking the list of attributes is not enough because the columns can be stable while the SELECT is not stable
-					//       Example: new way to compute the friendly name
-					//       The correct comparison algorithm is to compare the queries,
-					//       by using "SHOW CREATE VIEW" (MySQL 5.0.1 required) or to look into INFORMATION_SCHEMA/views
-					//       both requiring some privileges
-					// Decision: to simplify, let's consider the views as being wrong anytime
-					// Rework the view
-					//
-					$oFilter = new DBObjectSearch($sClass, '');
-					$oFilter->AllowAllData();
-					$sSQL = $oFilter->MakeSelectQuery();
-					$aMessages[$sClass] = "Redeclare view '$sView' (systematic - to support an eventual change in the friendly name computation)";
-					$aSugFix[$sClass][] = "ALTER VIEW `$sView` AS $sSQL";
+				// Create the view
+				//
+				$oFilter = new DBObjectSearch($sClass, '');
+				$oFilter->AllowAllData();
+				$sSQL = $oFilter->MakeSelectQuery();
+				// N°3431 - Need to remove class name from attribute alias to conform with 2.6 style
+				$aSQL = preg_split("@\n@", $sSQL);
+				$aResult = [];
+				$sState = 'start';
+				foreach ($aSQL as $sLine) {
+					$sLine = trim($sLine);
+					switch ($sState) {
+						case 'start':
+							if ($sLine == 'SELECT') {
+								$aResult[] = $sLine;
+								$sState = 'select';
+							}
+							break;
+						case 'select':
+							if ($sLine == 'FROM') {
+								$aResult[] = $sLine;
+								$sState = 'end';
+							} else {
+								if (preg_match("@^(?<left>.*) AS `{$sClass}(?<right>.*)`(?<sep>,?)$@", $sLine, $aMatches)) {
+									$sOldStyle = $aMatches['left'].' AS `'.$aMatches['right'].'`'.$aMatches['sep'];
+									$aResult[] = $sOldStyle;
+								} else {
+									$aResult[] = $sLine;
+								}
+							}
+							break;
+						case 'end':
+							$aResult[] = $sLine;
+							break;
+					}
 				}
-				else
-				{
-					// Create the view
-					//
-					$oFilter = new DBObjectSearch($sClass, '');
-					$oFilter->AllowAllData();
-					$sSQL = $oFilter->MakeSelectQuery();
-					$aMessages[$sClass] = "Missing view for class: $sClass";
-					$aSugFix[$sClass][] = "DROP VIEW IF EXISTS `$sView`";
-					$aSugFix[$sClass][] = "CREATE VIEW `$sView` AS $sSQL";
-				}
+				$sSQL = implode(" ", $aResult);
+
+				$aMessages[$sClass] = "Creating view for class: $sClass";
+				$aSugFix[$sClass][] = "DROP VIEW IF EXISTS `$sView`";
+				$aSugFix[$sClass][] = "CREATE VIEW `$sView` AS $sSQL";
 			}
 
 			foreach ($aSugFix as $sClass => $aQueries)
